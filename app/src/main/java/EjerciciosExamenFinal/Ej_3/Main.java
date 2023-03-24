927ead7556ad77e45c9629dd73638ca168c85727
Jose Carrasco <jsce2021@gmail.com>
2023-03-23 20:08:42 -0400

    Commit Final 2 Main

--- app/src/main/java/EjerciciosExamenFinal/Ej_3/Main.java
+++ app/src/main/java/EjerciciosExamenFinal/Ej_3/Main.java
@@ -1,7 +1,27 @@
 package EjerciciosExamenFinal.Ej_3;
 
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Colegio colegio = new Colegio();
+        colegio.setCapacidad(10);
+        Vector<Estudiante> estudiantes = new Vector<>();
+        estudiantes.add(new Estudiante("e - 1"));
+        estudiantes.add(new Estudiante("e - 2"));
+        estudiantes.add(new Estudiante("e - 3"));
+        colegio.setEstudiantes(estudiantes);
+        colegio.setNombre("Colegio 1");
+        Vector<RedSocial> redSocials = new Vector<>();
+        redSocials.add(new Facebook());
+        redSocials.add(new Twitter());
+        redSocials.add(new Whatsapp());
+        colegio.setRedesSociales(redSocials);
+        colegio.setRequisitos("REQUISITOS");
+
+        colegio.datosSistema(0);
+        colegio.datosSistema(1);
+        colegio.datosSistema(2);
+
     }
 }
